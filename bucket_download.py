923ba859ec1dca64081741cc843d8f7224f2f444
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2019-09-08 20:35:24 -0400

    added correct import

--- bucket_download.py
+++ bucket_download.py
@@ -1,9 +1,11 @@
+from google.cloud import storage
+
 bucket_name = 'deepholdem-amysantiago-bucket'
 prefix = 'street-2/'
 dl_dir = '/root/forkholdem/Data/TrainSamples/NoLimit/flop/'
 
 storage_client = storage.Client()
-bucket = storage_client.get_bucket(bucket_name=bucket_name)
+bucket = storage_client.get_bucket(bucket_name)
 blobs = bucket.list_blobs(prefix=prefix)  # Get list of files
 for blob in blobs:
     filename = blob.name.replace('/', '_') 
