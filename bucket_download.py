a428e6a87c82cb9fcd9e15ee0f0a67f63fc9cccb
Sadman Hasan'git config --global user.name Sadman <sadmanhassan08@gmail.com'git config --global user.email sadmanhassan08@gmail.comgit config --global user.email "sadmanhassan08@gmail.com>
2019-09-12 18:47:32 -0400

    .

--- bucket_download.py
+++ bucket_download.py
@@ -6,5 +6,5 @@ dl_dir = '/root/forkholdem/flop-nonconverted.tar.gz'
 
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
-blob = bucket.list_blob(prefix=prefix)  # Get list of files
+blob = bucket.blob(prefix)  # Get list of files
 blob.download_to_filename(dl_dir)  # Download
